9858c18e101d4d871873caed867060580ab5805e
tobias <tobiasleongzhunmun@gmail.com>
2018-10-23 00:26:20 +0800

    edit consumer.py to write to csv

--- consumer.py
+++ consumer.py
@@ -1,9 +1,21 @@
 from kafka import KafkaConsumer
+import csv
+from datetime import datetime
+import os
+
 
 consumer = KafkaConsumer('test')
+if os.path.isfile('messages.csv'):
+	print("messages.csv already exists...\ndeleting...")
+	os.remove('messages.csv')
 
-for message in consumer:
-	if len(message) >= 10:
-		print("Message is long.\n\nMessage:\n", message)
-	else:
-		print("Message is short.\n\nMessage:\n", message)
+with open('messages.csv', 'a') as f:
+	fWriter = csv.writer(f)
+	fWriter.writerow(['timestamp','message'])
+	print("CSV initialised")
+	for message in consumer:
+		msg = str(message.value.decode())
+		# divide by 1000 as it's in ms
+		ts = datetime.fromtimestamp(message.timestamp/1000).strftime("%A, %B %d, %Y %I:%M:%S")
+		print("Writing message %s..." % msg)
+		fWriter.writerow([ts, msg])
